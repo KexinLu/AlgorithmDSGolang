2c5f6ae4064ec9740342e4d445c7102423929cf1
Kexin Lu <langhxs@gmail.com>
2022-01-16 22:28:51 -0800

    add simple test

--- Algorithm/Array/3sum_lc15/lc15_test.go
+++ Algorithm/Array/3sum_lc15/lc15_test.go
@@ -1,6 +1,21 @@
 package lc15
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
-func testThreeSum(t *testing.T) {
+func TestThreeSum(t *testing.T) {
+	input := []int{-1, 0, 1, 2, -1, -4}
+	rst := threeSumTwoPointer(input)
+
+	exp := [][]int{[]int{-1, -1, 2}, []int{-1, 0, 1}}
+	if !arrEqual(exp, rst) {
+		t.Errorf("expected %v is different from actual %v", exp, rst)
+	}
+
+}
+
+func arrEqual(ar1, ar2 [][]int) bool {
+	return fmt.Sprint(ar1) == fmt.Sprint(ar2)
 }
